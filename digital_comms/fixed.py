6b8a3bdb680cf2735702a13f26c519a5249de893
Will Usher <william.usher@ouce.ox.ac.uk>
2017-02-24 16:13:56 +0000

    Added docstrings, refactored and added better aggregation features

--- digital_comms/fixed.py
+++ digital_comms/fixed.py
@@ -1,19 +1,138 @@
+"""The fixed broadband model
+
+"""
+
 import csv
 
-def read_in_exchange(file_path):
+class Exchange(object):
+    """An asset which serves fixed broadband premises
+
+    An Exchange is associated with a `geotype`, an area `code`.
+
+    The areas served by an Exchange are disaggregated into Bands, each of
+    which uses a technology. Average connection speed at the premises is a
+    function of the technology.
+
+    Arguments
+    ---------
+    location: dict
+        Contains 'oslaua', 'oscty', 'gor', 'code', 'geotype_name', 'geotype_number'
     """
 
-    {'CLBER': {'prem_under_1km': 4576, 
-               'prem_1_3km': 13728,
-               'prem_over_3km': 4576},
-               },
-               {'location': {'oslaua':,
-                             'oscty':,	
-                             'gor':	,
-                             'code': }}
+    def __init__(self, premises, speeds, location):
+
+        self.bands = []
+
+        for premise, speed in zip(premises, speeds):
+            self.bands.append(Band(premise, speed))
+
+        self.location = location
+
+    def weighted_premises(self):
+        """Calculates the proportion of premises per band for the exchange
+
+        Returns
+        -------
+        list
+            A list, of length ``len(self.bands)`` containing the proportion of
+            premises held in each band
+        """
+        total_premises = sum([band.premises for band in self.bands])
+
+        weights = [band.premises / total_premises for band in self.bands]
+
+        return weights
+
+    def average_speed_per_exchange(self):
+        """Computes weighted average of speeds at the premesis per Exchange
+
+        Returns
+        -------
+        float
+        """
+        average_speed = sum([weight * band.speed \
+            for (weight, band) in zip(self.weighted_premises(), self.bands)])
+
+        return average_speed
+
+    def update_band_technology(self, band_id, technology):
+        """Update the technology in a band
+
+        Arguments
+        ---------
+        band_id: int
+            The number of the band to update (1 to 3)
+        technology: str
+            The name of a valid technology ('fttp' or 'gfast')
+        """
+        self.bands[band_id - 1].technology = technology
+
+
+class Band(object):
+    """A `Band` is an area surrounding an exchange
+
+    A `Band` collects premises of similar connection speeds
+    around an `Exchange`
+
+    Parameters
+    ----------
+    premises: int
+        The number of premises served in the `Band`
+    current_speed: float
+        The existing average connection speed of the premises in the `Band`
+
+    Attributes
+    ----------
+    speed: float
+        The connection speed available to premises in this `Band`
+    """
+
+    def __init__(self, premises, current_speed):
+        self._technology = 'current'
+        self.premises = int(premises)
+        self.speed = current_speed
+
+    @property
+    def technology(self):
+        """The technology type currently available in the `Band`
+ 
+        Changing the technology affects the average connection speed.
+        """
+        return self._technology
+
+    @technology.setter
+    def technology(self, technology):
+        self._technology = technology
+        if self._technology == 'fttp':
+            self.speed = 2000
+        elif self._technology == 'gfast':
+            self.speed = 300
+
+
+
+def read_in_exchange(file_path):
+    """Reads in the exchanges and regions from the exchange data file
+
+    Parameters
+    ----------
+    file_path: str
+        The file path of the csv file containing the exchange data
+
+    Returns
+    -------
+    exchange : dict
+        A dictionary of :class:`Exchange` objects
+    code : dict
+        Maps exchanges to codes
+    geotype : dict
+        Maps exchanges to geotypes.  For examples::
+
+            geotypes = {1: ['CLBER', 'CLBIS', 'CLCAN']}
 
     """
     exchange = {}
+    code = {}
+    geotype = {}
 
     with open(file_path) as csvfile:
         reader = csv.DictReader(csvfile)
@@ -38,116 +157,83 @@ def read_in_exchange(file_path):
                                                  speeds,
                                                  locations)
 
-    return exchange
+            if row['code'] not in code:
+                code[row['code']] = []
+            else:
+                code[row['code']].append(row['exchange'])
+
+            if row['geotype_number'] not in geotype:
+                geotype[row['geotype_number']] = []
+            else:
+                geotype[row['geotype_number']].append(row['exchange'])
+
+    return exchange, code, geotype
 
 
-class Exchange(object):
+
+
+
+def apply_interventions_to_exchanges(decisions, exchanges, geotypes):
     """
 
     Arguments
     ---------
-    location: dict
-        Contains 'oslaua', 'oscty', 'gor', 'code', 'geotype_name', 'geotype_number'
+    decisions: list
+        A list of intervention dicts
+    exchanges: dict
+        The dictionary of exchanges
+    geotypes: dict
+        The dictionary of exchanges mapped to geotypes
     """
-
-    def __init__(self, premises, speeds, location):
-
-        self.bands = []
-
-        for premise, speed in zip(premises, speeds):
-            self.bands.append(Band(premise, speed))
-
-        self.location = location
-
-    def average_speed_per_exchange(self):
-        """
-        """
-
-        total_premises = 0
-        total_speed = 0
-
-        total_premises = sum([band.premises for band in self.bands])
-
-        proportions = [band.premises / total_premises for band in self.bands] 
-
-        proportions = []
-        for band in self.bands:
-            proportions.append(band.premises / total_premises)
-
-        average_speed = sum([weight * band.speed for (weight, band) in zip(proportions, self.bands)])
-
-        return average_speed
-
-    def update_band_technology(self, band_id, technology):
-        """Update the technology in a band
-
-        Arguments
-        ---------
-        band_id: int
-            The number of the band to update (1 to 3)
-        technology: str
-            The name of a valid technology ('fttp' or 'gfast')
-        """
-        self.bands[band_id - 1].set_technology(technology)
-
-
-class Band(object):
-
-    def __init__(self, premises, current_speed):
-        self.technology = 'current'
-        self.premises = int(premises)
-        self.speed = current_speed
-    
-    def set_technology(self, technology):
-        self.technology = technology
-        if self.technology == 'fttp':
-            self.speed = 2000
-        elif self.technology == 'gfast':
-            self.speed = 300
-
-
-def apply_interventions_to_exchanges(decisions, exchanges):
 
     for decision in decisions:
         # Find the matching exchanges with the same geotype
         technology, geotype, band = decision['name'].split("_")
-        for id, exchange in exchanges.items():
-            if exchange.location['geotype_number'] == geotype:
-                exchange.update_band_technology(int(band), technology)
-    
+
+        for exchange_id in geotypes[geotype]:
+
+        # for id, exchange in exchanges.items():
+        #     if exchange.location['geotype_number'] == geotype:
+            exchanges[exchange_id].update_band_technology(int(band), technology)
+
 def calculate_speed(exchanges):
     """For each OFCOM geography (code), calculate the average speed
-
-    """
-    results = []
-    for id, exchange in exchanges.items():
-        results.append({'speed': exchange.average_speed_per_exchange(),
-                        'code': exchange.location['code']})
-    return results
-
-def aggregate_by_region(results, region_heading='code'):
-    """Calculated running mean 
 
     Returns
     -------
     dict
-        Returns a dictionary aggregated `results` by `region_heading` e.g. {'00London': 50}
+        Returns a dictionary, where the exchange id is the key, and the
+        average speed is the value
+    """
+    results = {}
+    for key, exchange in exchanges.items():
+        results[key] = exchange.average_speed_per_exchange()
+    return results
+
+def aggregate_by_region(results, aggregation):
+    """Calculates average over an aggregated set of exchanges
+
+    Arguments
+    ---------
+    results : dict
+        A dictionary with exchange id as keys and values as values
+    aggregation: dict
+        A dict with aggregation heading as key, and list of exchange ids as values
+
+    Returns
+    -------
+    dict
+        Returns a dictionary aggregated `results` by `aggregation` e.g. {'00London': 50}
 
     """
-    calc_spd = {}
+    aggregated_results = {}
 
-    counts = {}
-    totals = {}
+    for heading, exchanges in aggregation.items():
+        total = sum([results[exchange] for exchange in exchanges])
+        count = len(exchanges)
+        aggregated_results[heading] = total / count
 
-    for row in results:
-        if row[region_heading] not in totals:
-            totals[row[region_heading]] = 0
-        if row[region_heading] not in counts:
-            counts[row[region_heading]] = 0
-        counts[row[region_heading]] += 1
-        totals[row[region_heading]] += row['speed']
-        calc_spd[row[region_heading]] = totals[row[region_heading]] / counts[row[region_heading]]
-    return calc_spd
+    return aggregated_results
 
 def run(decisions, file_path='data/exchanges.csv'):
     """
@@ -162,13 +248,13 @@ def run(decisions, file_path='data/exchanges.csv'):
     """
     results = {}
 
+    exchanges, codes, geotypes = read_in_exchange(file_path)
 
-    exchanges = read_in_exchange(file_path)
-    apply_interventions_to_exchanges(decisions, exchanges)
+    apply_interventions_to_exchanges(decisions, exchanges, geotypes)
 
     speed = calculate_speed(exchanges)
 
-    results['average_speed'] = aggregate_by_region(speed)
+    results['average_speed'] = aggregate_by_region(speed, codes)
     # energy_demand = calculate_energy_demand(exchanges)
     # premises_passed_by_tech = calculate_premises_passed(exchanges)
 
--- tests/test_fixed.py
+++ tests/test_fixed.py
@@ -37,68 +37,58 @@ def test_exchange_only_two_fttp():
 def test_apply_interventions():
 
     data = [{'name': 'gfast_1_1',
-             'capital_cost':
-                    {'value': 2201,
-                    'units': '£(million)'},
-                'economic_lifetime':
-                { 'value': 15,
-                    'units': 'years'},
-                'operational_life':
-                    {'value': 15,
-                    'units': 'years'},
-                'capacity':
-                    {'value': 2000,
-                    'units': 'Mbps'},
-                'location': 'Great Britain'}]
+             'capital_cost': {'value': 2201,
+                              'units': '£(million)'},
+             'economic_lifetime': {'value': 15,
+                                   'units': 'years'},
+             'operational_life': {'value': 15,
+                                  'units': 'years'},
+             'capacity': {'value': 2000,
+                          'units': 'Mbps'},
+             'location': 'Great Britain'}]
 
     filepath = './tests/fixtures/test_exchange.csv'
-    exchanges = read_in_exchange(filepath)
+    exchanges, codes, geotypes = read_in_exchange(filepath)
 
-    apply_interventions_to_exchanges(data, exchanges)
+    apply_interventions_to_exchanges(data, exchanges, geotypes)
 
     # assert 0
 
 def test_calculate_speed():
 
     filepath = './tests/fixtures/test_exchange.csv'
-    exchanges = read_in_exchange(filepath)
+    exchanges, codes, geotypes = read_in_exchange(filepath)
 
-    actual = calculate_speed(exchanges)    
-    expected = [{'speed': 50, 'code': '00London'},
-                {'speed': 50, 'code': '00London'},
-                {'speed': 50, 'code': '00London'}]
+    actual = calculate_speed(exchanges)
+
+    expected = {'CLBER': 50,
+                'CLBIS': 50,
+                'CLCAN': 50}
 
     assert actual == expected
 
 def test_calc_aggregate():
 
-    results = [{'speed': 50, 'code': '00London'},
-               {'speed': 50, 'code': '00London'},
-               {'speed': 50, 'code': '00London'}]
+    results = {'CLBER': 50,
+               'CLBIS': 50,
+               'CLCAN': 50}
 
-    actual = aggregate_by_region(results, region_heading='code')
+    aggregation = {'00London': ['CLBER', 'CLBIS', 'CLCAN']}
+    actual = aggregate_by_region(results, aggregation)
 
-    expected =  {'00London': 50}
+    expected = {'00London': 50}
 
     assert actual == expected
 
 def test_run_model():
 
     data = [{'name': 'gfast_1_1',
-             'capital_cost':
-                {'value': 2201,
-                'units': '£(million)'},
-             'economic_lifetime':
-               { 'value': 15,
-                'units': 'years'},
-             'operational_life':
-                {'value': 15,
-                 'units': 'years'},
-             'capacity':
-                {'value': 2000,
-                 'units': 'Mbps'},
+             'capital_cost': {'value': 2201, 'units': '£(million)'},
+             'economic_lifetime': {'value': 15, 'units': 'years'},
+             'operational_life': {'value': 15, 'units': 'years'},
+             'capacity': {'value': 2000, 'units': 'Mbps'},
              'location': 'Great Britain'}]
-    
+
     actual = run(data, './tests/fixtures/test_exchange.csv')
     expected = {'average_speed': {'00London': 100}}
 
