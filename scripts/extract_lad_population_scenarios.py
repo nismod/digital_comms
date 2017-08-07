d18f6956ac9c578cd49ed81131f3979e6c6d12a7
Tom Russell <tomalrussell@gmail.com>
2017-08-07 20:16:52 +0100

    Merged population of merged LADs
    When extracting population scenario data, sum values for population where
    LADs have been merged (e.g. to unitary authorities) while also matching old
    to new-style LAD ids (00AA => E09000001)

--- scripts/extract_lad_population_scenarios.py
+++ scripts/extract_lad_population_scenarios.py
@@ -10,9 +10,8 @@ BASE_PATH = CONFIG['file_locations']['base_path']
 INPUT_FILENAME = os.path.join(BASE_PATH, 'scenario_data', 'population_population_regional_breakdown.csv')
 LAD_CODE_FILENAME = os.path.join(BASE_PATH, 'source_data', 'lad_codes.csv')
 LAD_INTERMEDIATE_CODE_FILENAME = os.path.join(BASE_PATH, 'source_data', 'intermediate_lad_codes.csv')
-# LAD_WALES_FILENAME = os.path.join(BASE_PATH, 'source_data', 'wales_lad_2011_clipped.csv')
-# LAD_ENGLAND_FILENAME = os.path.join(BASE_PATH, 'source_data', 'england_lad_2011_clipped.csv')
 SCENARIOS = ("High", "Baseline", "Low")
+YEARS = range(2015, 2031)
 
 # Read in LAD code lookup (old code => new code)
 # header: LAD16CD,LAD16CDO,LAD16NM (i.e. new code, old code, name)
@@ -36,32 +35,44 @@ missing_codes = []
 
 # Read population scenarios file (from ITRC projections),
 # outputting total population (both genders, all ages) for each LAD
-output_files = {}
-output_writers = {}
+pop_by_scenario_year_lad = {scenario: {year: {} for year in YEARS} for scenario in SCENARIOS}
+
 with open(INPUT_FILENAME, 'r') as input_file:
     r = csv.DictReader(input_file)
-    for scenario in SCENARIOS:
-        output_filename = os.path.join(
-            BASE_PATH,
-            'scenario_data',
-            'population_{}_lad.csv'.format(scenario.lower()))
-        output_files[scenario] = open(output_filename, 'w', newline='')
-        output_writers[scenario] = csv.writer(output_files[scenario])
 
     for line in r:
         scenario = line['scenario']
-        if scenario in SCENARIOS and line['gender'] == '2':
-            pop = line['cat_ages_total']
+        year = int(line['year'])
+        if scenario in SCENARIOS and line['gender'] == '2' and year in YEARS:
             old_lad_code = line['location']
             if old_lad_code not in lad_code_lookup:
                 if old_lad_code not in missing_codes:
                     missing_codes.append(old_lad_code)
                     print(old_lad_code)
                 continue
-            lad = lad_code_lookup[old_lad_code]
-            year = line['year']
-            output_writers[scenario].writerow((year,lad,pop))
 
-# Close output files
-for output_file in output_files.values():
-    output_file.close()
+            # lookup new code for LAD
+            lad = lad_code_lookup[old_lad_code]
+            # convert from 1000s of people to simple count:
+            pop = float(line['cat_ages_total']) * 1000
+
+            # some old LADs were merged, so add values to get new region
+            if lad in pop_by_scenario_year_lad[scenario][year]:
+                pop_by_scenario_year_lad[scenario][year][lad] += pop
+            else:
+                pop_by_scenario_year_lad[scenario][year][lad] = pop
+
+output_files = {}
+output_writers = {}
+for scenario in SCENARIOS:
+    output_filename = os.path.join(
+        BASE_PATH,
+        'scenario_data',
+        'population_{}_lad.csv'.format(scenario.lower()))
+    print('Writing', output_filename)
+    with open(output_filename, 'w', newline='') as output_file:
+        w = csv.writer(output_file)
+        w.writerow(('year', 'lad', 'population'))
+        for year, pop_by_lad in pop_by_scenario_year_lad[scenario].items():
+            for lad, pop in pop_by_lad.items():
+                w.writerow((year, lad, pop))
