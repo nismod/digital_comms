2d4ee38a484d6e18c21f38f549e55e2735951939
RoaldL <roald.lemmen@ouce.ox.ac.uk>
2018-01-15 13:14:22 +0000

    Add example of fixture and unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,88 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-    Dummy conftest.py for digital_comms.
-
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    https://pytest.org/latest/plugins.html
-"""
 from __future__ import print_function, absolute_import, division
 
 import pytest
+from pytest import fixture
+
+@fixture(scope='function')
+def setup_lad():
+    return [
+        {
+            "id": 1,
+            "name": "Cambridge",
+        }
+    ]
+
+@fixture(scope='function')
+def setup_pcd_sector():
+    return [
+        {
+            "id": "CB11",
+            "lad_id": 1,
+            "population": 500,
+            "area": 2,
+            "user_throughput": 2
+        },
+        {
+            "id": "CB12",
+            "lad_id": 1,
+            "population": 200,
+            "area": 2,
+            "user_throughput": 2
+        }
+    ]
+
+@fixture(scope='function')
+def setup_assets():
+    return [
+        {
+            "pcd_sector": "CB11",
+            "site_ngr": 100,
+            "technology": "LTE",
+            "type": "macrocell_site",
+            "frequency": "800",
+            "bandwidth": "2x10MHz",
+            "build_date": 2017
+        },
+        {
+            "pcd_sector": "CB12",
+            "site_ngr": 200,
+            "technology": "LTE",
+            "type": "macrocell_site",
+            "frequency": "2600",
+            "bandwidth": "2x10MHz",
+            "build_date": 2017
+        }
+    ]
+
+@fixture(scope='function')
+def setup_capacity_lookup():
+    return {
+        ("Urban", "700", "2x10MHz"): [
+            (0, 1),
+            (1, 2),
+        ],
+        ("Urban", "800", "2x10MHz"): [
+            (0, 1),
+            (1, 2),
+        ],
+        ("Urban", "2600", "2x10MHz"): [
+            (0, 3),
+            (3, 5),
+        ],
+        ("Urban", "3500", "2x10MHz"): [
+            (0, 3),
+            (3, 5),
+        ],
+        ('Small cells', '3700', '2x25MHz'): [
+            (0, 3),
+            (3, 5),
+        ]
+    }
+
+@fixture(scope='function')
+def setup_clutter_lookup():
+    return [
+        (5, "Urban")
+    ]
--- tests/test_ccam.py
+++ tests/test_ccam.py
@@ -1,7 +1,46 @@
-"""Skeleton test
+"""Cambridge Communications Assessment Model Test
 """
 
-def test_pass():
-    """Always pass
-    """
-    pass
+from digital_comms.ccam import ICTManager, LAD, PostcodeSector
+
+class TestICTManager():
+
+    def test_create(self, setup_lad, setup_pcd_sector, setup_assets, 
+                    setup_capacity_lookup, setup_clutter_lookup):
+
+        Manager = ICTManager(setup_lad, setup_pcd_sector, setup_assets, 
+                             setup_capacity_lookup, setup_clutter_lookup)
+
+class TestLAD():
+
+    def test_create(self, setup_lad):
+
+        testLAD = LAD(setup_lad[0])
+
+        assert testLAD.id == setup_lad[0]['id']
+        assert testLAD.name == setup_lad[0]['name']
+    
+    def test_property_population(self, setup_lad, setup_pcd_sector, setup_assets, setup_capacity_lookup, setup_clutter_lookup):
+
+        testLAD = LAD(setup_lad[0])
+
+        # No postcode sectors
+        testPopulation = testLAD.population
+        assert testPopulation == 0
+
+        # Create a PostcodeSector with a population of 500
+        testPostcode = PostcodeSector(setup_pcd_sector[0], setup_assets, setup_capacity_lookup, setup_clutter_lookup)
+        testLAD.add_pcd_sector(testPostcode)
+
+        testPopulation = testLAD.population
+        assert testPopulation == 500
+
+        # Create a PostcodeSector with a population of 700
+        testPostcode = PostcodeSector(setup_pcd_sector[1], setup_assets, setup_capacity_lookup, setup_clutter_lookup)
+        testLAD.add_pcd_sector(testPostcode)
+
+        testPopulation = testLAD.population
+        assert testPopulation == 700
+
+
+
