a4991c47fdc7dfacbc2438c6d0de35dc9920131c
Edward Oughton <edward.oughton@gmail.com>
2018-03-06 20:07:28 +0000

    dissolve all postcodes to obtain exchange area boundaries

--- scripts/fixed_broadband_dissolve_postcode_shapes_for_exchange_areas.py
+++ scripts/fixed_broadband_dissolve_postcode_shapes_for_exchange_areas.py
@@ -25,60 +25,14 @@ SYSTEM_OUTPUT_FILENAME = os.path.join(BASE_PATH, 'processed')
 # Initialize timer
 start = time.time()
 
-#with fiona.open(os.path.join(SYSTEM_INPUT_FIXED, 'exchange_boundary_polygons.shp'), 'r') as source:
-
-#     # Write exchange polygons
-#     sink_schema = {}
-#     sink_schema['geometry'] = 'MultiPolygon'
-#     sink_schema['properties'] = OrderedDict()
-#     sink_schema['properties']['EX_ID'] = 'str:8'
-
-#     with fiona.open(
-#         os.path.join(SYSTEM_OUTPUT_FILENAME, 'exchange_boundary_polygons_dissolved.shp'),
-#         'w',
-#         crs=source.crs,
-#         driver=source.driver,
-#         schema=sink_schema,
-#         ) as sink:
-
-#         exchange_areas = {}
-
-#         # Loop through all exchanges
-#         for f in source:
-
-#             # Convert Multipolygons to list of polygons
-#             if (isinstance(shape(f['geometry']), MultiPolygon)):
-#                 polygons = [p.buffer(0) for p in shape(f['geometry'])]
-#             else:
-#                 polygons = [shape(f['geometry'])]
-
-#             # Extend list of geometries, create key (exchange_id) if non existing
-#             try:
-#                 exchange_areas[f['properties']['EX_ID']].extend(polygons)
-#             except:
-#                 exchange_areas[f['properties']['EX_ID']] = []
-#                 exchange_areas[f['properties']['EX_ID']].extend(polygons)
-
-#         # Write Multipolygons per exchange
-#         for exchange, area in exchange_areas.items():
-
-#             exchange_multipolygon = MultiPolygon(area)
-
-#             s = {}
-#             s['geometry'] = mapping(exchange_multipolygon)
-#             s['properties'] = OrderedDict()
-#             s['properties']['EX_ID'] = exchange
-
-#             sink.write(s)
-
 with fiona.open(os.path.join(SYSTEM_INPUT_FIXED, 'exchange_boundary_polygons.shp'), 'r') as source:
     # preserve the schema of the original shapefile, including the crs
-    
+
     # Write exchange polygons
     sink_schema = {}
     sink_schema['geometry'] = 'Polygon'
     sink_schema['properties'] = OrderedDict()
-    sink_schema['properties']['EX_ID'] = 'str:8' 
+    sink_schema['properties']['EX_ID'] = 'str:8'
 
     with fiona.open(
         os.path.join(SYSTEM_OUTPUT_FILENAME, 'exchange_boundary_polygons_dissolved.shp'),
