00f104678fafde7896895d42ebd3252e45ff0ed0
ej550 <edward.oughton@gmail.com>
2018-05-17 11:22:00 +0100

    created geotype allocation script

--- scripts/geotype_allocation.py
+++ scripts/geotype_allocation.py
@@ -16,13 +16,16 @@ CONFIG.read(os.path.join(os.path.dirname(__file__), 'script_config.ini'))
 BASE_PATH = CONFIG['file_locations']['base_path']
 
 #####################################
-# setup file locations and data files
+# SETUP FILE LOCATIONS
 #####################################
 
 SYSTEM_INPUT_FIXED = os.path.join(BASE_PATH, 'raw')
 SYSTEM_OUTPUT_FILENAME = os.path.join(BASE_PATH, 'processed')
 SYSTEM_INPUT_NETWORK = os.path.join(SYSTEM_INPUT_FIXED, 'network_hierarchy_data')
 
+#####################################
+# SPECIFY FUNCTIONS
+#####################################
 
 def read_exchange_boundaries():
     with fiona.open(os.path.join(SYSTEM_OUTPUT_FILENAME, '_exchange_areas.shp'), 'r') as source:
@@ -32,11 +35,16 @@ def read_postcode_boundaries():
 
     postcode_shapes = []
     
-    for dirpath, subdirs, files in os.walk(SYSTEM_INPUT_FIXED, 'codepoint', 'codepoint-poly_2429451', 'cambridge_city'):
+    for dirpath, subdirs, files in os.walk(os.path.join(SYSTEM_INPUT_FIXED, 'codepoint', 'codepoint-poly_2429451', 'cambridge_city')):
         for x in files:
+            #print(files)
             if x.endswith(".shp"):
                 with fiona.open(os.path.join(dirpath, x), 'r') as source:
                     postcode_shapes.extend([boundary for boundary in source])
+
+    for postcode in postcode_shapes:
+            centroid = shape(postcode['geometry']).centroid
+            postcode['geometry'] = mapping(centroid)
 
     return postcode_shapes
 
@@ -73,93 +81,120 @@ def add_codepoint_lut_to_postcode_shapes(data, lut):
         del codepoint_lut_data[area['POSTCODE']]['POSTCODE']
 
     # Add properties
-    for datum in data:
+    for datum in data:        
         if datum['properties']['POSTCODE'] in codepoint_lut_data:
-            print(datum)
             datum['properties'].update({
-                'premises': codepoint_lut_data[datum['properties']['POSTCODE']]['delivery_points']
+                'delivery_points': codepoint_lut_data[datum['properties']['POSTCODE']]['delivery_points']
             })
         else:
             datum['properties'].update({
-                'premises': 0, 
+                'delivery_points': 0, 
             })
     
     return data
+
+############################################
+# ADD INTERSECTING EXCHANGE IDs TO POSTCODES
+############################################
+
+def add_exchange_to_postcodes(postcodes, exchanges):
+    
+    joined_postcodes = []
+
+    # Initialze Rtree
+    idx = index.Index()
+
+    for rtree_idx, postcode in enumerate(postcodes):
+        idx.insert(rtree_idx, shape(postcode['geometry']).bounds, postcode)
+
+    # Join the two
+    for exchange in exchanges:
+        for n in idx.intersection((shape(exchange['geometry']).bounds), objects=True):
+            exchange_shape = shape(exchange['geometry'])
+            postcode_shape = shape(n.object['geometry'])
+            if exchange_shape.contains(postcode_shape):
+                n.object['properties']['id'] = exchange['properties']['id']
+                joined_postcodes.append(n.object)
+
+    return joined_postcodes
 
 #####################################
 # SUM PREMISES BY EXCHANGE
 #####################################
 
-# # group premises by lads
-# premises_per_lad = defaultdict(list)
-
-# for premise in self._premises:
-#     """
-#     'Cambridge': [
-#         premise1,
-#         premise2
-#     ]
-#     """
-#     premises_per_lad[premise.lad].append(premise)
-
-
-# # run statistics on each lad
-# coverage_results = defaultdict(dict)
-# for lad in premises_per_lad.keys():
-
-#     # return dict that looks like
-#     """
-#     dict of dicts
-#     'Cambridge' : {
-#         'premise_with_fttp': int, 
-#         'premise_with_fttdp': int, 
-#         'premise_with_fttc': int, 
-#         'premise_with_adsl': int, 
-#         'premise_with_cable': int,
-#     },
-#     'Oxford' : ..
-#     """
-
-#     #print(lad)
-#     sum_of_fttp = sum([premise.fttp for premise in premises_per_lad[lad]]) # contain  list of premises objects in the lad
-#     sum_of_gfast = sum([premise.gfast for premise in premises_per_lad[lad]]) # contain  list of premises objects in the lad
-#     sum_of_fttc = sum([premise.fttc for premise in premises_per_lad[lad]]) # contain  list of premises objects in the lad
-#     sum_of_adsl = sum([premise.adsl for premise in premises_per_lad[lad]]) # contain  list of premises objects in the lad
+def sum_premises_by_exchange():
     
-#     sum_of_premises = len(premises_per_lad[lad]) # contain  list of premises objects in the lad
+    #group premises by lads
+    premises_per_exchange = defaultdict(list)
 
-#     coverage_results[lad] = {
-#         'percentage_of_premises_with_fttp': round(sum_of_fttp / sum_of_premises, 2),
-#         'percentage_of_premises_with_gfast': round(sum_of_gfast / sum_of_premises, 2),
-#         'percentage_of_premises_with_fttc': round(sum_of_fttc / sum_of_premises, 2),
-#         'percentage_of_premises_with_adsl': round(sum_of_adsl / sum_of_premises, 2)
-#     }
+    for postcode in postcode_centroids:
+        """
+        'exchange1': [
+            postcode1,
+            postcode2
+        ]
+        """
+        #print(postcode)
+        premises_per_exchange[postcode['properties']['id']].append(postcode['properties']['delivery_points'])
 
-# return coverage_results
+    # run statistics on each lad
+    premises_results = defaultdict(dict)
+    for exchange in premises_per_exchange.keys():
+
+        #print(lad)
+        sum_of_delivery_points = sum([premise for premise in premises_per_exchange[exchange]]) # contain  list of premises objects in the lad
+
+        if sum_of_delivery_points >= 20000:
+            geotype = '>20k lines'
+
+        elif sum_of_delivery_points >= 10000 and sum_of_delivery_points < 20000:
+            geotype = '>10k lines'            
+
+        elif sum_of_delivery_points >= 3000 and sum_of_delivery_points <= 10000:
+            geotype = '>3k lines'     
+
+        elif sum_of_delivery_points >= 1000 and sum_of_delivery_points <= 30000:
+            geotype = '>1k lines' 
+
+        elif sum_of_delivery_points < 1000:
+            geotype = '<1k lines' 
+
+        premises_results[exchange] = {
+            'delivery_points': sum_of_delivery_points,
+            'geotype': geotype
+        }
+
+    return premises_results
+
+##########################################
+# CONVERT DATA INTO LIST OF DICT STRUCTURE 
+##########################################
+
+def covert_data_into_list_of_dicts():
+    my_data = []
+
+    # output and report results for this timestep
+    for exchange in premises_by_exchange:
+        my_data.append({
+        'exchange_id': exchange,
+        'delivery_points': premises_by_exchange[exchange]['delivery_points'],
+        'geotype': premises_by_exchange[exchange]['geotype']
+        })
+
+    return my_data
 
 #####################################
-# WRITE LUTS/ASSETS/LINKS
+# WRITE DATA - SINGLE FILE FOR ALL
 #####################################
 
-def write_shapefile(data, path):
-
-    # Translate props to Fiona sink schema
-    prop_schema = []
-    for name, value in data[0]['properties'].items():
-        fiona_prop_type = next((fiona_type for fiona_type, python_type in fiona.FIELD_TYPES_MAP.items() if python_type == type(value)), None)
-        prop_schema.append((name, fiona_prop_type))
-
-    sink_driver = 'ESRI Shapefile'
-    sink_crs = {'init': 'epsg:27700'}
-    sink_schema = {
-        'geometry': data[0]['geometry']['type'],
-        'properties': OrderedDict(prop_schema)
-    }
-
-    # Write all elements to output file
-    with fiona.open(os.path.join(SYSTEM_OUTPUT_FILENAME, path), 'w', driver=sink_driver, crs=sink_crs, schema=sink_schema) as sink:
-        for feature in data:
-            sink.write(feature)
+def csv_writer(data, output_fieldnames, filename):
+    """
+    Write data to a CSV file path
+    """
+    with open(os.path.join(SYSTEM_OUTPUT_FILENAME, filename), 'w') as csv_file:
+        writer = csv.DictWriter(csv_file, output_fieldnames, lineterminator = '\n')
+        writer.writeheader()
+        writer.writerows(data)
 
 ################################################
 # run scripts
@@ -169,15 +204,26 @@ print("reading exchange boundaries")
 exchange_boundaries = read_exchange_boundaries()
 
 print("reading postcode boundaries")
-postcode_boundaries = read_postcode_boundaries()
+postcode_centroids = read_postcode_boundaries()
 
 print("reading codepoint lut")
 codepoint_lut = read_codepoint_lut()
 
 print("adding codepoint lut to postcode shapes")
-postcode_boundaries = add_codepoint_lut_to_postcode_shapes(postcode_boundaries, codepoint_lut)
+postcode_centroids = add_codepoint_lut_to_postcode_shapes(postcode_centroids, codepoint_lut)
 
-#print(codepoint_lut)
+print("adding intersecting exchange IDs to postcode points")
+postcode_centroids = add_exchange_to_postcodes(postcode_centroids, exchange_boundaries)
 
-print("write exchanges")
-write_shapefile(postcode_boundaries, 'postcode_geotype_shapes.shp')
+print("summing delivery points by exchange area")
+premises_by_exchange = sum_premises_by_exchange()
+
+print("convert exchange areas to list of dicts")
+premises_by_exchange = covert_data_into_list_of_dicts()
+
+# # Write LUTs
+print('write geotype lut')
+geotype_lut_fieldnames = ['exchange_id', 'delivery_points', 'geotype']
+csv_writer(premises_by_exchange, geotype_lut_fieldnames, 'exchange_geotype_lut.csv')
+
+print("script finished")
