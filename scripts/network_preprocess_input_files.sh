331f2b2fec01c7236e44de97c4f0013133f8dbc6
ej550 <edward.oughton@gmail.com>
2018-11-19 18:42:54 +0000

    add new path

--- scripts/network_preprocess_input_files.sh
+++ scripts/network_preprocess_input_files.sh
@@ -3,9 +3,10 @@
 echo Pre-processing $1: Running on `hostname`
 
 source activate digital_comms
+cd /ouce-home/projects/mistral/nismod/digital_comms2/
 
-cd /soge-home/projects/mistral/nismod/digital_comms2/data/digital_comms/intermediate
-mkdir -p $1
-touch $1/log.txt
+cd /soge-home/projects/mistral/nismod/digital_comms2
+mkdir -p data/digital_comms/intermediate/$1
+touch data/digital_comms/intermediate/$1/log.txt
 
 python `pwd`/scripts/network_preprocess_input_files.py $1 &> data/processed_cluster/$1/log.txt
