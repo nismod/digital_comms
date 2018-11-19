b4ba98fee2e319b79c114897487cf2ffcaed67aa
ej550 <edward.oughton@gmail.com>
2018-11-19 17:53:41 +0000

    update network_preprocess to add .txt files to intermediate folder

--- scripts/network_preprocess_input_files.sh
+++ scripts/network_preprocess_input_files.sh
@@ -5,7 +5,8 @@ echo Pre-processing $1: Running on `hostname`
 source activate digital_comms
 cd projects/digital_comms
 
-mkdir -p data/processed_cluster/$1
-touch data/processed_cluster/$1/log.txt
+cd /soge-home/projects/mistral/nismod/digital_comms
+mkdir -p data/digital_comms/intermediate/$1
+touch data/digital_comms/intermediate/$1/log.txt
 
 python `pwd`/scripts/network_preprocess_input_files.py $1 &> data/processed_cluster/$1/log.txt
