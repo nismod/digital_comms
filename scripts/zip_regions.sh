5ca338cc4507808eedb50ea789dff954091fda3d
ej550 <edward.oughton@gmail.com>
2018-11-27 14:25:11 +0000

    add zip_regions.sh, remove unnecessary file writing for speed

---
+++ scripts/zip_regions.sh
@@ -0,0 +1,14 @@
+cd /ouce-home/projects/mistral/nismod/digital_comms2/data/digital_comms/intermediate/
+
+cat east.txt | zip -r@ east_zipped.zip
+cat east_midlands.txt | zip -r@ east_midlands_zipped.zip
+cat london.txt | zip -r@ london_zipped.zip
+cat north_east.txt | zip -r@ north_east_zipped.zip
+cat north_west.txt | zip -r@ north_west_zipped.zip
+cat scotland.txt | zip -r@ scotland_zipped.zip
+cat south_east.txt | zip -r@ south_east_zipped.zip
+cat south_west.txt | zip -r@ south_west_zipped.zip
+cat wales.txt | zip -r@ wales_zipped.zip
+cat west_midlands.txt | zip -r@ west_midlands_zipped.zip
+cat yorkshire.txt | zip -r@ yorkshire_zipped.zip
+
