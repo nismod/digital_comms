60407b2bde698e16204816622ad43c835c7315b7
ej550 <edward.oughton@gmail.com>
2019-01-08 11:20:27 +0000

    update = to ==

--- scripts/run_parallel.sh
+++ scripts/run_parallel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-exchanges=`python scripts/network_cluster_input_files.py $1`
+exchanges == `python scripts/network_cluster_input_files.py $1`
 
 #seq 1 $count | parallel -n1 --no-notice ./plot_stations.py {}
 parallel --sshloginfile nodeslist -n1 --no-notice --progress `pwd`/scripts/network_preprocess_input_files.sh ::: $exchanges
