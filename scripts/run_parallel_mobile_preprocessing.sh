550626c3c999cfff336fc15594a6948f3438cfbc
Tom Russell <tomalrussell@gmail.com>
2019-02-04 10:12:44 +0000

    Pipe args to parallel in mobile preprocessing

--- scripts/run_parallel_mobile_preprocessing.sh
+++ scripts/run_parallel_mobile_preprocessing.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-pcd_sectors=`python scripts/mobile_cluster_input_files.py $1`
 
 #seq 1 $count | parallel -n1 --no-notice ./plot_stations.py {}
 #parallel --sshloginfile nodeslist -n1 --no-notice --progress `pwd`/digital_comms/mobile_network/transmitter_module.sh ::: $exchanges
 
-echo pcd_sectors
-
-python `pwd`/digital_comms/mobile_network/transmitter_module.py ::: $pcd_sectors
+python scripts/mobile_cluster_input_files.py $1 | \
+    parallel python `pwd`/digital_comms/mobile_network/transmitter_module.py {}
