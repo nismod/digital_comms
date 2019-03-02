b0d640f3e8b17f22c5cdae10847ed8ce945b8de2
ej550 <edward.oughton@gmail.com>
2019-03-02 13:16:33 +0000

    Correct cluster .sh script directory structure

--- scripts/network_preprocess_input_files.sh
+++ scripts/network_preprocess_input_files.sh
@@ -3,10 +3,10 @@
 echo Pre-processing $1: Running on `hostname`
 
 source activate digital_comms
-cd /ouce-home/projects/mistral/nismod/digital_comms2/
+cd /ouce-home/projects/mistral/nismod/digital_comms/
 
-#cd /soge-home/projects/mistral/nismod/digital_comms2
-mkdir -p /soge-home/projects/mistral/nismod/digital_comms2/data/digital_comms/intermediate/$1
-touch /soge-home/projects/mistral/nismod/digital_comms2/data/digital_comms/intermediate/$1/log.txt
+#cd /soge-home/projects/mistral/nismod/digital_comms
+mkdir -p /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/$1
+touch /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/$1/log.txt
 
-python `pwd`/scripts/network_preprocess_input_files.py $1 &> /soge-home/projects/mistral/nismod/digital_comms2/data/digital_comms/intermediate/$1/log.txt
+python `pwd`/scripts/network_preprocess_input_files.py $1 &> /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/$1/log.txt
