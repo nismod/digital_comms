15f1cda6a344902c79751d08bb92a178def6e347
ej550 <edward.oughton@gmail.com>
2019-03-16 09:34:21 +0000

    - Write unit test for adoption.py. - Get adoption.py working correctly

--- digital_comms/fixed_network/adoption.py
+++ digital_comms/fixed_network/adoption.py
@@ -8,56 +8,64 @@ LOGGER = getLogger(__name__)
 
 def update_adoption_desirability(system, annual_adoption_rate):
     """
-    Given adoption parameters and a premises, return likely adoptees
+    Given adoption parameters and a set of distribution points, return likely adoptees.
 
-    scenario - scenario name
-    number_of_adopters - aggregate number wanting to adopt
-    premises - premises objects
-    timestep - year
+    Utilises wta variable (Willingness To Adopt).
 
-    1) access list of wta for all premises
-    2) rank premises based on wta
-    3) select top n % of premises in ranking
-    4) if premises in top n % adoption_desirability = True
+    1) access list of summed wta for all premises at the distribution point level.
+    2) rank distribution points based on wta
+    3) select top n % of new premises wanting to adopt
 
     """
+
+    #Step 1
+    #Work out the number of raw new premises wanting to adopt in a single year
+    distributions_already_wanting_to_adopt = [
+        distribution for distribution in system._distributions
+        if distribution.adoption_desirability is True
+    ]
+
+    #get total premises needing to be served in current system
+    count_of_premises_already_wanting_to_adopt = 0
+    for distribution in distributions_already_wanting_to_adopt:
+        count_of_premises_already_wanting_to_adopt += distribution.total_prems
+
+    #get total premises needing to be served in current system
+    total_premises = 0
+    for distribution in system._distributions:
+        total_premises += distribution.total_prems
+
+    #get raw annual premises adoption
+    raw_annual_premises_adoption = round(total_premises*(annual_adoption_rate/100))
+
+    #get raw new premises wanting to adopt
+    raw_new_premises_wanting_to_adopt = (
+        raw_annual_premises_adoption - count_of_premises_already_wanting_to_adopt)
 
     distributions_not_wanting_to_adopt = [
         distribution for distribution in system._distributions
         if distribution.adoption_desirability is not True
     ]
 
-    distributions_already_wanting_to_adopt = [
-        distribution for distribution in system._distributions
-        if distribution.adoption_desirability is True
-    ]
-
     #rank distributions based on household wta
     distributions_not_wanting_to_adopt = sorted(
-        distributions_not_wanting_to_adopt, key=lambda item: item.wta)
-    
-    #get total premises needing to be served in current system
-    total_premises = 0
-    for distribution in system._distributions:
-        total_premises += distribution.total_prems
+        distributions_not_wanting_to_adopt, key=lambda item: item.wta, reverse=True)
 
-    raw_annual_premises_adoption = round(total_premises*(annual_adoption_rate/100))
-    
     new_distributions_wanting_to_adopt = []
     premises_wanting_to_adopt = 0
-    for distribution in system._distributions:
-        if premises_wanting_to_adopt + distribution.total_prems <= raw_annual_premises_adoption:
+    for distribution in distributions_not_wanting_to_adopt:
+        if premises_wanting_to_adopt + distribution.total_prems <= raw_new_premises_wanting_to_adopt:
             new_distributions_wanting_to_adopt.append(distribution)
             premises_wanting_to_adopt += distribution.total_prems
         else:
             break
 
-    LOGGER.debug("-- premises not wanting to connect %s", 
+    LOGGER.debug("-- premises not wanting to connect %s",
         len([dist.total_prems for dist in distributions_not_wanting_to_adopt]))
-    LOGGER.debug("-- premises wanting to connect %s", 
+    LOGGER.debug("-- premises wanting to connect %s",
         sum([dist.total_prems for dist in new_distributions_wanting_to_adopt]))
     LOGGER.debug("-- total premises %s",
-        sum([dist.total_prems for dist in distributions_already_wanting_to_adopt]) + 
+        sum([dist.total_prems for dist in distributions_already_wanting_to_adopt]) +
         sum([dist.total_prems for dist in distributions_not_wanting_to_adopt]))
 
     distribution_adoption = []
