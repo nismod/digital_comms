5d92306d246fd0dbf4307b31c4a5e76355c5161f
ej550 <edward.oughton@gmail.com>
2019-04-09 21:06:44 +0100

    update path

--- scripts/run_parallel_mobile_preprocessing_lads.sh
+++ scripts/run_parallel_mobile_preprocessing_lads.sh
@@ -4,4 +4,4 @@
 #parallel --sshloginfile nodeslist -n1 --no-notice --progress `pwd`/digital_comms/mobile_network/transmitter_module.sh ::: $exchanges
 
 python scripts/mobile_cluster_lad_input_files.py $1 | \
-    parallel python `pwd`/digital_comms/digital_comms/scripts/mobile_preprocess_lookup_tables.py {}
+    parallel python `pwd`/scripts/mobile_preprocess_lookup_tables.py {}
