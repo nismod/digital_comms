c1e00ca43ef66c41f2d6e39c158ae48b92976a01
Edward Oughton <edward.oughton@gmail.com>
2019-05-12 16:34:11 +0100

    Revert "Feature/update mobile simulation"

--- tests/mobile_network/test_mobile_interventions.py
+++ tests/mobile_network/test_mobile_interventions.py
@@ -1,15 +1,5 @@
+"""Skeleton test
 """
-Test Mobile Network interventions.py 
-
-11th May 2019
-
-Written by Edward J. Oughton
-
-"""
-from digital_comms.mobile_network.interventions import decide_interventions
-from digital_comms.mobile_network.model import PostcodeSector
-
-
 
 def test_pass():
     """Always pass
--- tests/mobile_network/test_mobile_model.py
+++ tests/mobile_network/test_mobile_model.py
@@ -1,109 +1,43 @@
+"""Cambridge Communications Assessment Model Test
 """
-Test Mobile Network model.py 
 
-11th May 2019
+from digital_comms.mobile_network.model import ICTManager, LAD, PostcodeSector
 
-Written by Edward J. Oughton
-
-"""
-from digital_comms.mobile_network.model import (
-    NetworkManager, LAD, PostcodeSector
-    )
-
-class TestNetworkManager():
+class TestICTManager():
 
     def test_create(self, setup_lad, setup_pcd_sector, setup_assets,
-                    setup_capacity_lookup, setup_clutter_lookup,
-                    setup_service_obligation_capacity,
-                    setup_traffic, setup_market_share):
+                    setup_capacity_lookup, setup_clutter_lookup):
 
-        Manager = NetworkManager(setup_lad, setup_pcd_sector, setup_assets,
-            setup_capacity_lookup, setup_clutter_lookup,
-            setup_service_obligation_capacity, 
-            setup_traffic, setup_market_share)
+        Manager = ICTManager(setup_lad, setup_pcd_sector, setup_assets,
+                             setup_capacity_lookup, setup_clutter_lookup)
 
 class TestLAD():
 
-    def test_create(self, setup_lad, setup_service_obligation_capacity):
+    def test_create(self, setup_lad):
 
-        testLAD = LAD(setup_lad[0], setup_service_obligation_capacity)
+        testLAD = LAD(setup_lad[0])
 
         assert testLAD.id == setup_lad[0]['id']
         assert testLAD.name == setup_lad[0]['name']
 
-    def test_property_population(self, setup_lad, setup_pcd_sector, 
-        setup_assets, setup_capacity_lookup, setup_clutter_lookup, 
-        setup_service_obligation_capacity, setup_traffic, 
-        setup_market_share):
+    def test_property_population(self, setup_lad, setup_pcd_sector, setup_assets, setup_capacity_lookup, setup_clutter_lookup):
 
-        testLAD = LAD(setup_lad[0], setup_service_obligation_capacity)
+        testLAD = LAD(setup_lad[0])
 
         # No postcode sectors
         testPopulation = testLAD.population
         assert testPopulation == 0
 
         # Create a PostcodeSector with a population of 500
-        testPostcode = PostcodeSector(setup_pcd_sector[0], setup_assets, 
-            setup_capacity_lookup, setup_clutter_lookup, 
-            setup_service_obligation_capacity,
-            setup_traffic, setup_market_share)
-
-        #test pcd_sector capacity
-        assert round(testPostcode.capacity,2) == 1.83
-
-        #test pcd_sector demand
-        testDemand = round(testPostcode.demand,2)
-        assert testDemand == 1.14
-
+        testPostcode = PostcodeSector(setup_pcd_sector[0], setup_assets, setup_capacity_lookup, setup_clutter_lookup)
         testLAD.add_pcd_sector(testPostcode)
 
         testPopulation = testLAD.population
         assert testPopulation == 500
 
         # Create a PostcodeSector with a population of 700
-        testPostcode = PostcodeSector(setup_pcd_sector[1], setup_assets, 
-            setup_capacity_lookup, setup_clutter_lookup,
-            setup_service_obligation_capacity, 
-            setup_traffic, setup_market_share)
-        
-        assert round(testPostcode.capacity,2) == 1.83
-
-        #test pcd_sector demand
-        testDemand = round(testPostcode.demand,2)
-        assert testDemand == 0.46
-
+        testPostcode = PostcodeSector(setup_pcd_sector[1], setup_assets, setup_capacity_lookup, setup_clutter_lookup)
         testLAD.add_pcd_sector(testPostcode)
 
         testPopulation = testLAD.population
         assert testPopulation == 700
-
-        testPopDensity = testLAD.population_density
-        assert testPopDensity == 175
-
-        testCapacity = testLAD.capacity()
-        #CB11, 800+2600MHz, density of 0.5 sites per km^2 
-        # Capacity is 1km^2 + 0.83km^2 = 1.83Mbps/km2  
-        #CB12 is the same, so (1.83+1.83)/2 = 1.83Mbps/km2 average capacity
-        assert round(testCapacity,2) == 1.83
-
-        #test lad demand
-        #2.28 = 1.14*2
-        #0.92 = 0.46*2
-        #0.8 = (2.28+0.92)/(2+2)
-        testDemand = round(testLAD.demand(),2)
-        assert testDemand == 0.8
-
-        testCoverage = testLAD.coverage()
-        assert testCoverage == 0
-
-
-
-# MANAGER = NetworkManager(LADS, PCD_SECTORS, ASSETS, CAPACITY_LOOKUP, 
-#     CLUTTER_LOOKUP, SERVICE_OBLIGATION_CAPACITY, TRAFFIC, MARKET_SHARE)
-
-# for lad in MANAGER.lads.values():
-#     pprint(lad)
-#     for pcd in lad._pcd_sectors.values():
-#         print(" ", pcd, "capacity:{:.2f} demand:{:.2f}".format(
-#             pcd.capacity, pcd.demand
-#             ))
