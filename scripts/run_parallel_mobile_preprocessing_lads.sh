a0060b94ba48e7620d0e15e23d43af6c71ac056c
ej550 <edward.oughton@gmail.com>
2019-05-15 07:08:04 +0100

    Add echo

--- scripts/run_parallel_mobile_preprocessing_lads.sh
+++ scripts/run_parallel_mobile_preprocessing_lads.sh
@@ -3,5 +3,7 @@
 #seq 1 $count | parallel -n1 --no-notice ./plot_stations.py {}
 #parallel --sshloginfile nodeslist -n1 --no-notice --progress `pwd`/digital_comms/mobile_network/transmitter_module.sh ::: $exchanges
 
+echo Pre-processing $1
+
 python scripts/mobile_cluster_lad_input_files.py $1 | \
     parallel python `pwd`/scripts/mobile_preprocess_lookup_tables.py {}
