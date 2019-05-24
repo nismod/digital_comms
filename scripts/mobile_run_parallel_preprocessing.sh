325d23e2c3bb2e48b052dcd2606b2a40f1db72bc
ej550 <edward.oughton@gmail.com>
2019-05-24 08:59:52 +0100

    Update mobile cluster script and runners

--- scripts/mobile_run_parallel_preprocessing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#seq 1 $count | parallel -n1 --no-notice ./plot_stations.py {}
-#parallel --sshloginfile nodeslist -n1 --no-notice --progress `pwd`/digital_comms/mobile_network/transmitter_module.sh ::: $exchanges
-
-python scripts/mobile_cluster_input_files.py $1 | \
-    parallel python `pwd`/digital_comms/mobile_network/transmitter_module.py {}
